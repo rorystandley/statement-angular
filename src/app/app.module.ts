05f5d12c3980d81b064c51a08e09308fdbefa019
Rory Standley <rorystandley@gmail.com>
2019-09-29 16:39:23 +0100

    Adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedCoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule( {
 	declarations: [
@@ -12,7 +13,8 @@ import { SharedCoreModule } from './core/core.module';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		SharedCoreModule
+		SharedCoreModule,
+		BrowserAnimationsModule
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
